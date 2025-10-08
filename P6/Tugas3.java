6ca39f80b2fc05f2f27b2a84997f10e02a43dcef
FafirruH <fafirruh@gmail.com>
2025-10-08 13:57:53 +0700

    tugas 3

--- P6/Tugas3.java
+++ P6/Tugas3.java
@@ -3,15 +3,22 @@ public class Tugas3 {
 
     public static void main(String[] args) {
         Scanner faf = new Scanner(System.in);
-        String ktm05,regis05;
-        System.out.print("Apakah Anda Membawa KTM : ");
-        ktm05=faf.next();
-        System.out.print("Apakah Anda Sudah Registrasi : ");
-        regis05=faf.next();
-        if (ktm05.equalsIgnoreCase("ya")&&regis05.equalsIgnoreCase("ya")) {
-            System.out.println("boleh masuk");
+        String user05;
+        int sks05;
+        System.out.println("Dosen/Mahasiswa");
+        user05=faf.next();
+        if (user05.equalsIgnoreCase("dosen")) {
+            System.out.println("akses diberikan");
+        }else if(user05.equalsIgnoreCase("mahasiswa")){
+            System.out.println("berapa sks anda : ");
+            sks05=faf.nextInt();
+            if (sks05>=12) {
+                System.out.println("akses diberikan");
+            }else{
+                System.out.println("akses ditolak");
+            }
         }else{
-            System.out.println("dilarang masuk");
+            System.out.println("akses ditolak");
         }
     }
 }
