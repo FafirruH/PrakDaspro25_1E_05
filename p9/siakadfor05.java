95b4c5db188e3c3526bf2bfee545e9161deb6872
FafirruH <fafirruh@gmail.com>
2025-10-28 13:59:28 +0700

    p9_percobaan1PertanyaanNo4

--- p9/siakadfor05.java
+++ p9/siakadfor05.java
@@ -1,24 +1,32 @@
 package p9;
+
 import java.util.Scanner;
 
 public class siakadfor05 {
     public static void main(String[] args) {
         Scanner faf = new Scanner(System.in);
-        double nilai05, tertinggi05=0,terendah05=100;
-        for (int i = 1; i <=10; i++){
-            System.out.print("Masukkan Nilai Mahasiswa ke - "+i+": ");
-            nilai05=faf.nextDouble();
-            if (nilai05>tertinggi05) {
-                tertinggi05=nilai05;
+        double nilai05, tertinggi05 = 0, terendah05 = 100;
+        int lulus05 = 0, tidakLulus05 = 0;
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan Nilai Mahasiswa ke - " + i + ": ");
+            nilai05 = faf.nextDouble();
+            if (nilai05 > tertinggi05) {
+                tertinggi05 = nilai05;
             }
-            if (nilai05<terendah05) {
-                terendah05=nilai05;                
+            if (nilai05 < terendah05) {
+                terendah05 = nilai05;
+            }
+            if (nilai05 >= 60) {
+                lulus05++;
+            } else {
+                tidakLulus05++;
             }
         }
-        System.out.println("Nilai Tertinggi Adalah "+tertinggi05);
-        System.out.println("Nilai Terendah Adalah "+terendah05);
+        System.out.println("Nilai Tertinggi Adalah " + tertinggi05);
+        System.out.println("Nilai Terendah Adalah " + terendah05);
+        System.out.println("Jumlah Mahasiswa Yang Lulus : " + lulus05);
+        System.out.println("Jumlah Mahasiswa Yang Tidak Lulus : " + tidakLulus05);
         faf.close();
     }
 
-    
 }
