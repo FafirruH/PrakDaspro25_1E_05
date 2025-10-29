afb0ea4a745588863275b9e873534e64fbcf7fd7
FafirruH <fafirruh@gmail.com>
2025-10-29 13:04:17 +0700

    P10_Percobaan4_PertanyaanNo2

--- p10/NilaiKelompok05.java
+++ p10/NilaiKelompok05.java
@@ -1,19 +1,29 @@
 package p10;
 import java.util.Scanner;
-public class NilaiKelompok05 {
+public class NilaiKelompok05{
     public static void main(String[] args) {
-        Scanner faf=new Scanner(System.in);
-        for (int i = 1; i <=6; i++) {
-            System.out.println("Kelompok ke-"+i);
-            float totalNilai05=0;
+        Scanner faf = new Scanner(System.in);
+        float RataTertinggi05 = 0;
+        int kelompokTertinggi05 = 0;
+        for (int i = 1; i <= 6; i++) {
+            System.out.println("Kelompok ke-" + i);
+            float totalNilai05 = 0;
             for (int j = 1; j <= 5; j++) {
-                System.out.print("/tNilai Dari Kelompok Penilai ke-"+j+": ");
-                int nilai05=faf.nextInt();
-                totalNilai05+=nilai05;
+                System.out.print("\tNilai dari Kelompok Penilai ke-" + j + ": ");
+                int nilai05 = faf.nextInt();
+                totalNilai05 += nilai05;
             }
-            float rataNilai05=totalNilai05/5;
-            System.out.println("Rata-Rata adalah : "+rataNilai05);
+            float rataNilai05 = totalNilai05 / 5;
+            System.out.println("Rata-rata adalah: " + rataNilai05);
+            System.out.println("-----------------------------------");
+            if (rataNilai05 > RataTertinggi05) {
+                RataTertinggi05 = rataNilai05;
+                kelompokTertinggi05 = i;
+            }
         }
+        System.out.println("=== HASIL AKHIR ===");
+        System.out.println("Kelompok dengan rata-rata tertinggi: Kelompok ke-" + kelompokTertinggi05);
+        System.out.println("Dengan nilai rata-rata: " + RataTertinggi05);
+        faf.close();
     }
-    
 }
