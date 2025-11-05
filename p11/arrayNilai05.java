f2c2a15957f5124efbc0c769170240134116d066
FafirruH <fafirruh@gmail.com>
2025-11-05 14:51:39 +0700

    P11_Percobaan2_Pertanyaan1

--- p11/arrayNilai05.java
+++ p11/arrayNilai05.java
@@ -4,7 +4,7 @@ public class arrayNilai05 {
     public static void main(String[] args) {
         Scanner faf = new Scanner(System.in);
         int[]nilaiAkhir05=new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir05.length; i++) {
             System.out.print("Masukkan Nilai Akhir ke- ");
             nilaiAkhir05[i]=faf.nextInt();
         }for (int i = 0; i < 10; i++) {
