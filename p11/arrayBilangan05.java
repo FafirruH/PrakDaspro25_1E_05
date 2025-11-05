e163aceb442d7a7243ea5de21180c7c1829ac7be
FafirruH <fafirruh@gmail.com>
2025-11-05 14:34:57 +0700

    P11_Percobaan1_Pertanyaan2

--- p11/arrayBilangan05.java
+++ p11/arrayBilangan05.java
@@ -2,11 +2,11 @@ package p11;
 
 public class arrayBilangan05 {
     public static void main(String[] args) {
-        int[] bil=new int[4];
-        bil[0]=5.0;
-        bil[1]=12867;
-        bil[2]=7.5;
-        bil[3]=2000000;
+        int[] bil={5,13,-7,17};
+        // bil[0]=5;
+        // bil[1]=13;
+        // bil[2]=-7;
+        // bil[3]=17;
         System.out.println(bil[0]);
         System.out.println(bil[1]);
         System.out.println(bil[2]);
