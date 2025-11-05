216143cf7aa927d23dcc82e9c69500d2a8758e70
FafirruH <fafirruh@gmail.com>
2025-11-05 14:38:07 +0700

    P11_Percobaan1_Pertanyaan3

--- p11/arrayBilangan05.java
+++ p11/arrayBilangan05.java
@@ -7,9 +7,8 @@ public class arrayBilangan05 {
         // bil[1]=13;
         // bil[2]=-7;
         // bil[3]=17;
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for (int i = 0; i < bil.length; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
