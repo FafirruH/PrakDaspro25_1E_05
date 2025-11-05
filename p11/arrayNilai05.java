80d1de29b4d04cba1fb375fb4c7bc78cd76d125f
FafirruH <fafirruh@gmail.com>
2025-11-05 14:49:08 +0700

    P11_Percobaan2

---
+++ p11/arrayNilai05.java
@@ -0,0 +1,16 @@
+package p11;
+import java.util.Scanner;
+public class arrayNilai05 {
+    public static void main(String[] args) {
+        Scanner faf = new Scanner(System.in);
+        int[]nilaiAkhir05=new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan Nilai Akhir ke- ");
+            nilaiAkhir05[i]=faf.nextInt();
+        }for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai Akhir ke- "+i+"Adalah "+nilaiAkhir05[i]);
+        }
+        faf.close();
+    }
+    
+}
