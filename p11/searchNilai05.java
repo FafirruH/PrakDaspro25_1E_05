6b23e3c39805365acfbb107673e098712fc528c8
FafirruH <fafirruh@gmail.com>
2025-11-07 17:55:08 +0700

    P11_Percobaan4

---
+++ p11/searchNilai05.java
@@ -0,0 +1,15 @@
+package p11;
+public class searchNilai05 {
+    public static void main(String[] args) {
+        int[]arrNilai05={80,85,78,96,90,82,86};
+        int key05=90;
+        int hasil05=0;
+        for (int i = 0; i < arrNilai05.length; i++) {
+            hasil05=i;
+            break;
+        }
+        System.out.println();
+        System.out.println("Nilai "+key05+" Ketemu Di Index ke-"+hasil05);
+    }
+    
+}
