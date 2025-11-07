8070654fc492fcda29c2099cd510f87131af7671
FafirruH <fafirruh@gmail.com>
2025-11-07 17:56:52 +0700

    P11_Percobaan4_Pertanyaan2

--- p11/searchNilai05.java
+++ p11/searchNilai05.java
@@ -1,15 +1,25 @@
 package p11;
+import java.util.Scanner;
 public class searchNilai05 {
     public static void main(String[] args) {
-        int[]arrNilai05={80,85,78,96,90,82,86};
-        int key05=90;
+        Scanner faf=new Scanner(System.in);
+        System.out.print("Masukkan Banyaknya Nilai Yang Akan Di Input : ");
+        int jumlah05=faf.nextInt();
+        int[]arrNilai05=new int[jumlah05];
         int hasil05=0;
         for (int i = 0; i < arrNilai05.length; i++) {
-            hasil05=i;
-            break;
+            System.out.print("Masukkan Nilai Mahasiswa ke-"+(i+1)+" : ");
+            arrNilai05[i]=faf.nextInt();
         }
-        System.out.println();
-        System.out.println("Nilai "+key05+" Ketemu Di Index ke-"+hasil05);
+        System.out.print("Masukkan Nilai Yang Ingin Dicari : ");
+        int key05=faf.nextInt();
+        for (int i = 0; i < arrNilai05.length; i++) {
+            if (arrNilai05[i]==key05) {
+                hasil05=i+1;
+                System.out.println("Nilai "+key05+" Ketemu, Merupakan Nilai Mahasiswa ke-"+hasil05);
+                break;
+            }
+        }
+        faf.close();
     }
-    
 }
