d6d0794e920834daf1b627adcb782d7d7e7b92c0
FafirruH <fafirruh@gmail.com>
2025-11-07 16:34:32 +0700

    P11_Percobaan3_Pertanyaan1

--- p11/arrayRataNilai05.java
+++ p11/arrayRataNilai05.java
@@ -6,15 +6,20 @@ public class arrayRataNilai05 {
         int[] nilaiMhs05=new int[10];
         double total05=0;
         double rata05;
+        int lulus05=0;
         for (int i = 0; i < nilaiMhs05.length; i++) {
             System.out.print("Masukkan Nilai Mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs05[i]=faf.nextInt();
         }
             for (int i = 0; i < nilaiMhs05.length; i++) {
                 total05+=nilaiMhs05[i];
+                if (nilaiMhs05[i]>70) {
+                    lulus05++;
+                }
             }
         rata05=total05/nilaiMhs05.length;
         System.out.println("Rata-rata Nilai = "+rata05);
+        System.out.println("Jumlah Mahasiswa Yang lulus : "+lulus05);
         faf.close();
     }
 }
