93b7940e8a42afe1656e96593c643f4a32743f27
FafirruH <fafirruh@gmail.com>
2025-11-12 11:11:24 +0700

    P12_Percobaan1_Pertanyaan3

--- P12/bioskop05.java
+++ P12/bioskop05.java
@@ -10,6 +10,7 @@ public class bioskop05 {
         penonton05[2][0]="Eka";
         penonton05[2][1]="Farhan";
         penonton05[3][0]="Gisel";
+        penonton05[3][0]="Hana" ;
         System.out.printf("%s \t %s\n",penonton05[0][0],penonton05[0][1]);
         System.out.printf("%s \t %s\n",penonton05[1][0],penonton05[1][1]);
         System.out.printf("%s \t %s\n",penonton05[2][0],penonton05[2][1]);
