a8d930c0aadfd3e2322685af8c9dce6a6cc732b8
FafirruH <fafirruh@gmail.com>
2025-11-12 11:08:00 +0700

    P12_Percobaan1

---
+++ P12/bioskop05.java
@@ -0,0 +1,18 @@
+package P12;
+
+public class bioskop05 {
+    public static void main(String[] args) {
+        String[][]penonton05=new String[4][2];
+        penonton05[0][0]="Amin";
+        penonton05[0][1]="Bena";
+        penonton05[1][0]="Candra";
+        penonton05[1][1]="Dela";
+        penonton05[2][0]="Eka";
+        penonton05[2][1]="Farhan";
+        penonton05[3][0]="Gisel";
+        System.out.printf("%s \t %s\n",penonton05[0][0],penonton05[0][1]);
+        System.out.printf("%s \t %s\n",penonton05[1][0],penonton05[1][1]);
+        System.out.printf("%s \t %s\n",penonton05[2][0],penonton05[2][1]);
+        System.out.printf("%s \t %s\n",penonton05[3][0],penonton05[3][1]);
+    }
+}
