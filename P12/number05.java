ba0d597fc2fab99d9be5908ed223b2fde368733f
FafirruH <fafirruh@gmail.com>
2025-11-12 15:01:37 +0700

    P12_Percobaan3_Pertanyaan1

--- P12/number05.java
+++ P12/number05.java
@@ -8,5 +8,8 @@ public class number05 {
         myNumber05[0]=new int[5];
         myNumber05[1]=new int[3];
         myNumber05[2]=new int[1];
+        for (int i = 0; i < myNumber05.length; i++) {
+            System.out.println(Arrays.toString(myNumber05[i]));
+        }
     }
 }
