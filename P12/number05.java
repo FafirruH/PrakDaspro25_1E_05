fc1c473130dfef33cfcbe80fcec87c2a3895ace1
FafirruH <fafirruh@gmail.com>
2025-11-12 15:01:04 +0700

    P12_Percobaan3

---
+++ P12/number05.java
@@ -0,0 +1,12 @@
+package P12;
+
+import java.util.Arrays;
+
+public class number05 {
+    public static void main(String[] args) {
+        int[][] myNumber05=new int[3][];
+        myNumber05[0]=new int[5];
+        myNumber05[1]=new int[3];
+        myNumber05[2]=new int[1];
+    }
+}
