8068794be4f62403a97fa308383809af73198370
FafirruH <fafirruh@gmail.com>
2025-11-12 13:53:11 +0700

    P12_Percobaan2_Pertanyaan2

--- P12/bioskopWithScanner05.java
+++ P12/bioskopWithScanner05.java
@@ -1,26 +1,66 @@
 package P12;
 import java.util.Scanner;
+
 public class bioskopWithScanner05 {
     public static void main(String[] args) {
-        Scanner faf=new Scanner(System.in);
-        int baris05,kolom05;
-        String nama05,next05;
-        String[][]penonton05=new String[4][2];
-        while (true) {
-            System.out.print("Masukkan Nama: ");
-            nama05=faf.nextLine();
-            System.out.print("Masukkan Baris: ");
-            baris05=faf.nextInt();
-            System.out.print("Masukkan Kolom: ");
-            kolom05=faf.nextInt();
+        Scanner faf = new Scanner(System.in);
+        String[][] penonton05 = new String[4][2];
+        int pilihan05;
+
+        do {
+            System.out.println("\nMENU BIOSKOP");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Keluar");
+            System.out.print("Pilih menu: ");
+            pilihan05 = faf.nextInt();
             faf.nextLine();
 
-            penonton05[baris05-1][kolom05-1]=nama05;
-            System.out.print("Input Penonton Lainnya ? (y/n) : ");
-            next05=faf.next();
-            if (next05.equalsIgnoreCase("n")) {
-                break;
+            switch (pilihan05) {
+                case 1:
+                    String nama05, next05;
+                    int baris05, kolom05;
+                    while (true) {
+                        System.out.print("Masukkan Nama: ");
+                        nama05 = faf.nextLine();
+                        System.out.print("Masukkan Baris (1-4): ");
+                        baris05 = faf.nextInt();
+                        System.out.print("Masukkan Kolom (1-2): ");
+                        kolom05 = faf.nextInt();
+                        faf.nextLine();
+                        penonton05[baris05 - 1][kolom05 - 1] = nama05;
+                        System.out.print("Input penonton lain? (y/n): ");
+                        next05 = faf.next();
+                        faf.nextLine();
+                        if (next05.equalsIgnoreCase("n")) {
+                            break;
+                        }
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("\nDAFTAR PENONTON");
+                    for (int i = 0; i < penonton05.length; i++) {
+                        for (int j = 0; j < penonton05[i].length; j++) {
+                            if (penonton05[i][j] == null) {
+                                System.out.print("***\t");
+                            } else {
+                                System.out.print(penonton05[i][j] + "\t");
+                            }
+                        }
+                        System.out.println();
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Program selesai.");
+                    break;
+
+                default:
+                    System.out.println("Pilihan tidak valid!");
             }
-        }
+        } while (pilihan05 != 3);
+
+        faf.close();
     }
 }
