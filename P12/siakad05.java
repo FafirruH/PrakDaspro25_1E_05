a30837f23418c575a97ea3e8b8dc4a652acf3cff
FafirruH <fafirruh@gmail.com>
2025-11-16 19:07:50 +0700

    P12_Percobaan5_no7

--- P12/siakad05.java
+++ P12/siakad05.java
@@ -6,10 +6,13 @@ public class siakad05 {
         int [][]nilai05=new int[4][3];
         for (int i = 0; i < nilai05.length; i++) {
             System.out.println("Input Nilai Mahasiswa ke-"+(i+1));
+            double totalPerSiswa05=0;
             for (int j = 0; j < nilai05[i].length; j++) {
                 System.out.print("Nilai Mata Kuliah "+(j+1)+": ");
                 nilai05[i][j]=faf.nextInt();
+                totalPerSiswa05+=nilai05[i][j];
             }
+            System.out.println("Nilai Rata-Rata: "+totalPerSiswa05/3);
         } 
         faf.close();
     }
