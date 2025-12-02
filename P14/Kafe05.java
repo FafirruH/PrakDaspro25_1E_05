ad681994d022b03679c4afc58e8e8cd1a3bbee53
FafirruH <fafirruh@gmail.com>
2025-12-02 10:45:57 +0700

    P14_Percobaan3

--- P14/Kafe05.java
+++ P14/Kafe05.java
@@ -1,7 +1,8 @@
 package P14;
-
+import java.util.Scanner;
 public class Kafe05 {
-    static void menu05(String namaPelanggan05, boolean isMember05, String kodepromo05) {
+    static Scanner faf = new Scanner(System.in);
+    public static void menu05(String namaPelanggan05, boolean isMember05, String kodepromo05) {
         System.out.println("Selamat Datang, " + namaPelanggan05 + "!");
         if (isMember05) {
             System.out.println("Anda Adalah Member, dapatkan diskon 10% untuk setiap pembelian");
@@ -24,8 +25,18 @@ public class Kafe05 {
         System.out.println("Silahkan pilih menu yang anda inginkan.");
 
     }
-
+    public static int hitungTotalHarga05(int pilihanMenu05,int banyakItem05){
+        int[]hargaItem05={15000,20000,22000,12000,10000,18000};
+        int hargaTotal05=hargaItem05[pilihanMenu05-1]*banyakItem05;
+        return hargaTotal05;
+    }
     public static void main(String[] args) {
         menu05("Budi", true,"DISKON30" );
+        System.out.print("\nMasukkan nomor menu yang Anda ingin pesan: ");
+        int pilihanMenu05=faf.nextInt();
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem05=faf.nextInt();
+        int totalHarga05=hitungTotalHarga05(pilihanMenu05, banyakItem05);
+        System.out.println("Total harga untuk pesanana Anda: Rp"+totalHarga05);
     }
 }
