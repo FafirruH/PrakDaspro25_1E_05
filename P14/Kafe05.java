9524f261b47fd4828376221f55636e616395693c
FafirruH <fafirruh@gmail.com>
2025-12-02 13:39:56 +0700

    P14_Percobaan3_Pertanyaan3

--- P14/Kafe05.java
+++ P14/Kafe05.java
@@ -2,14 +2,14 @@ package P14;
 import java.util.Scanner;
 public class Kafe05 {
     static Scanner faf = new Scanner(System.in);
-    public static void menu05(String namaPelanggan05, boolean isMember05, String kodepromo05) {
+    public static void menu05(String namaPelanggan05, boolean isMember05, String kodePromo05) {
         System.out.println("Selamat Datang, " + namaPelanggan05 + "!");
         if (isMember05) {
             System.out.println("Anda Adalah Member, dapatkan diskon 10% untuk setiap pembelian");
         }
-        if (kodepromo05.equals("DISKON50")) {
+        if (kodePromo05.equals("DISKON50")) {
             System.out.println("Kode Promo: Diskon 50%");
-        } else if (kodepromo05.equals("DISKON30")) {
+        } else if (kodePromo05.equals("DISKON30")) {
             System.out.println("Kode Promo: Diskon 30%");
         } else {
             System.out.println("Kode Promo: INVALID");
@@ -23,20 +23,30 @@ public class Kafe05 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silahkan pilih menu yang anda inginkan.");
-
     }
-    public static int hitungTotalHarga05(int pilihanMenu05,int banyakItem05){
-        int[]hargaItem05={15000,20000,22000,12000,10000,18000};
-        int hargaTotal05=hargaItem05[pilihanMenu05-1]*banyakItem05;
+    public static int hitungTotalHarga05(int pilihanMenu05, int banyakItem05, String kodePromo05) {
+        int[] hargaItem05 = {15000, 20000, 22000, 12000, 10000, 18000};
+        if (pilihanMenu05 < 1 || pilihanMenu05 > 6) {
+            System.out.println("Menu tidak valid!");
+            return 0;
+        }
+        int hargaTotal05 = hargaItem05[pilihanMenu05 - 1] * banyakItem05;
+        if (kodePromo05.equalsIgnoreCase("DISKON50")) {
+            hargaTotal05 *= 0.5;
+        } else if (kodePromo05.equalsIgnoreCase("DISKON30")) {
+            hargaTotal05 *= 0.7;
+        } else {
+            System.out.println("Kode Promo Invalid");
+        }
         return hargaTotal05;
     }
-    public static void main(String[] args) {
-        menu05("Budi", true,"DISKON30" );
+        public static void main(String[] args) {
+        menu05("Budi", true, "DISKON50");
         System.out.print("\nMasukkan nomor menu yang Anda ingin pesan: ");
-        int pilihanMenu05=faf.nextInt();
+        int pilihanMenu05 = faf.nextInt();
         System.out.print("Masukkan jumlah item yang ingin dipesan: ");
-        int banyakItem05=faf.nextInt();
-        int totalHarga05=hitungTotalHarga05(pilihanMenu05, banyakItem05);
-        System.out.println("Total harga untuk pesanana Anda: Rp"+totalHarga05);
+        int banyakItem05 = faf.nextInt();
+        int totalHarga05 = hitungTotalHarga05(pilihanMenu05, banyakItem05, "DISKON50");
+        System.out.println("Total harga untuk pesanan Anda: Rp" + totalHarga05);
     }
 }
