d4710256780423d827273e7ef5ea36891f9d49af
FafirruH <fafirruh@gmail.com>
2025-12-02 15:30:17 +0700

    P14_Percobaan5

--- P14/HitungBalok05.java
+++ P14/HitungBalok05.java
@@ -1,6 +1,14 @@
 package P14;
 import java.util.Scanner;
 public class HitungBalok05 {
+    static int HitungLuas05(int pjg,int lb){
+        int Luas05=pjg*lb;
+        return Luas05;
+    }
+    static int HitungVolume05(int Tinggi,int a,int b){
+        int Volume05 = HitungLuas05(a,b)*Tinggi;
+        return Volume05;
+    }
     public static void main(String[] args) {
         Scanner faf=new Scanner(System.in);
         int p05,l05,t05,L05,vol05;
@@ -10,9 +18,9 @@ public class HitungBalok05 {
         l05=faf.nextInt();
         System.out.print("Masukkan Tinggi : ");
         t05=faf.nextInt();
-        L05=p05*l05;
+        L05=HitungLuas05(p05,l05);
         System.out.println("Luas Persegi Panjang adalah "+L05);
-        vol05=p05*l05*t05;
+        vol05=HitungVolume05(t05,p05,l05);
         System.out.println("Volume balok adalah "+vol05);
         faf.close();
     }
