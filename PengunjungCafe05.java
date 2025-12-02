5ecd01985eecb66cd31fcb8f9d24ae3d967804ae
FafirruH <fafirruh@gmail.com>
2025-12-02 14:22:30 +0700

    P14_Percobaan4_Pertanyaan2

--- PengunjungCafe05.java
+++ PengunjungCafe05.java
@@ -1,8 +1,8 @@
 public class PengunjungCafe05 {
     static void daftarPengunjung05(String...namaPengunjung05){
         System.out.println("Daftar nama pengunjung: ");
-        for (int i = 0; i < namaPengunjung05.length; i++) {
-            System.out.println("- "+namaPengunjung05);
+        for (String nama05 : namaPengunjung05) {
+            System.out.println("- "+nama05);
         }
     }
     public static void main(String[] args) {
